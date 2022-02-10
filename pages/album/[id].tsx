0d87a74253ae529cdfe3b8cd3be7e8dec90f1299
Sander <sander@notanumber.digital>
2022-02-10 12:55:25 +0100

    remove prisma - testing plesk

--- pages/album/[id].tsx
+++ pages/album/[id].tsx
@@ -1,78 +1,78 @@
-import React, { useState } from "react";
-// import prisma from '../../db/prisma';
+// import React, { useState } from "react";
+// // import prisma from '../../db/prisma';
 
-import { Album } from "@prisma/client";
-import Card from "../../components/Card";
-import { useRouter } from "next/router";
-import { prismaData } from "../../db/prismaData";
+// import { Album } from "@prisma/client";
+// import Card from "../../components/Card";
+// import { useRouter } from "next/router";
+// import { prismaData } from "../../db/prismaData";
 
-export const getServerSideProps = async (context) => {
-  const album = await prismaData.album.one(context.params.id);
+// export const getServerSideProps = async (context) => {
+//   const album = await prismaData.album.one(context.params.id);
 
-  return {
-    props: {
-      album,
-    },
-  };
-};
+//   return {
+//     props: {
+//       album,
+//     },
+//   };
+// };
 
-const AlbumDetail = ({ album }) => {
-  const [fields, setFields] = useState<Album>(album);
-  const router = useRouter();
+// const AlbumDetail = ({ album }) => {
+//   const [fields, setFields] = useState<Album>(album);
+//   const router = useRouter();
 
-  const updateRecord = async (e: any) => {
-    e.preventDefault();
+//   const updateRecord = async (e: any) => {
+//     e.preventDefault();
 
-    const response = await fetch(
-      `../api/album/${fields.id}`,
-      {
-        method: "PATCH",
-        body: JSON.stringify(fields),
-      }
-    );
-    if (!response.ok) throw new Error(response.statusText);
+//     const response = await fetch(
+//       `../api/album/${fields.id}`,
+//       {
+//         method: "PATCH",
+//         body: JSON.stringify(fields),
+//       }
+//     );
+//     if (!response.ok) throw new Error(response.statusText);
 
-    e.target.reset();
-    const updated = await response.json();
+//     e.target.reset();
+//     const updated = await response.json();
 
-    return updated;
-  };
+//     return updated;
+//   };
 
-  const deleteRecord = async () => {
-    console.log(album.id);
+//   const deleteRecord = async () => {
+//     console.log(album.id);
 
-    const response = await fetch(`../api/album/${fields.id}`, {
-      method: "DELETE",
-    });
+//     const response = await fetch(`../api/album/${fields.id}`, {
+//       method: "DELETE",
+//     });
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
 
-    if (response.ok) router.push("/") && console.log(`deleted ${album.id}`);
+//     if (response.ok) router.push("/") && console.log(`deleted ${album.id}`);
 
-    return await response.json();
-  };
-  return (
-    <div>
-      <button onClick={() => history.back()}>Back</button>
-      <Card content={fields} />
+//     return await response.json();
+//   };
+//   return (
+//     <div>
+//       <button onClick={() => history.back()}>Back</button>
+//       <Card content={fields} />
 
-      <div>
-        <form onSubmit={updateRecord}>
-          <input
-            type="text"
-            name="record"
-            id="record"
-            placeholder="Update Name"
-            onChange={(e) => setFields({ ...fields, record: e.target.value })}
-          />
-          <input type="submit" value="update" />
-        </form>
-        <button onClick={deleteRecord}>Delete</button>
-      </div>
-    </div>
-  );
-};
+//       <div>
+//         <form onSubmit={updateRecord}>
+//           <input
+//             type="text"
+//             name="record"
+//             id="record"
+//             placeholder="Update Name"
+//             onChange={(e) => setFields({ ...fields, record: e.target.value })}
+//           />
+//           <input type="submit" value="update" />
+//         </form>
+//         <button onClick={deleteRecord}>Delete</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default AlbumDetail;
+// export default AlbumDetail;
